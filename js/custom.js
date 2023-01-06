613a0a04835e98ffdb65fafa32f697a8aaa44ae9
Sajidul-Is1am <mds040772@gmail.com>
2023-01-06 23:44:57 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+// details part small img to conver big img start
+var mainImg = document.getElementById("mainImg");
+var smallimg = document.getElementsByClassName("small_img")
+
+smallimg[0].onclick=function(){
+    mainImg.src = smallimg[0].src;
+}
+smallimg[01].onclick = function () {
+    mainImg.src = smallimg[01].src;
+}
+smallimg[2].onclick = function () {
+    mainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function () {
+    mainImg.src = smallimg[3].src;
+}
+// details part small img to conver big img end
